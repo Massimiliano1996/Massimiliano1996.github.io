d42a46aa191c5324e1b18cf4f2cc46479641253a
unknown <massimiliano.carboni96@gmail.com>
2022-01-16 18:55:27 +0100

    Comma fixing

--- src/components/Home/Worlds.jsx
+++ src/components/Home/Worlds.jsx
@@ -24,7 +24,7 @@ export default function Worlds(props) {
             
 
                     <Stars radius={300} />
-                    <pointLight position={2, 0, 2} intensity={1} />
+                    <pointLight position={2 } intensity={1} />
                     <mesh ref={earthRotation}>
                         <OrbitControls enableRotate={true} rotateSpeed={0.4} enablePan={true} panSpeed={0.5} enableZoom={true} maxZoom={3} zoomSpeed={1} />
                         <sphereGeometry args={[1.5, 40, 40]} />
