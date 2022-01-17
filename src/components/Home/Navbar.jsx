4de23fe69717703621f896a4f7f7bd7f7b3add7b
Massimiliano1996 <massimiliano.carboni96@gmail.com>
2022-01-17 15:13:23 +0100

    Real Fixing

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,25 +1,20 @@
-<<<<<<< HEAD
 
 
-=======
-import styled from 'styled-components'
->>>>>>> 24f090e268184445d8ca105491faa2ff8d73faa1
 
 
 
 import React from 'react'
-<<<<<<< HEAD
 import styled from 'styled-components'
 
 export default function Navbar() {
 
     const NavConteiner=styled.div`
     display:flex;
-    height:3rem;
+    height:2rem;
     background-color:none;
     color:white;
-
-
+    justify-content:space-around;
+    padding:1rem;
 
 
         `
@@ -30,13 +25,13 @@ export default function Navbar() {
 
         <NavConteiner>
                 
-            <ul style={{}}>
-                <li> Servizio Meteo </li>
-                <li>Servizio Localizzazione</li>
+           
+                <span> Servizio Meteo </span>
+                <span>Servizio Localizzazione</span>
 
 
 
-            </ul>
+            
         
             
         
@@ -47,33 +42,5 @@ export default function Navbar() {
         
             
         
-=======
-
-export default function Navbar() {
-
-const MyNav=styled.div`
-
-display:flex;
-background-color:black;
-height:2rem;
-color:white;
-justify-content:space-around;
-text-decoration:bold;
-padding:1rem;
-`
-
-
-
-
-    return (
-        <MyNav>
-            <span>Servizio Meteo</span>
-            <span>Servizio Geolocalizzazione</span>
-
-
-
-
-        </MyNav>
->>>>>>> 24f090e268184445d8ca105491faa2ff8d73faa1
     )
 }
