d68a3b42a01533266709302e44c4bad98231fa5c
unknown <massimiliano.carboni96@gmail.com>
2022-01-29 01:21:38 +0100

    Display Meteo

---
+++ src/components/ServizioMeteo/MeteoCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MeteoCard(prop) {
+  return (
+    <>
+  <h1>{prop.main}</h1>
+  <p>{prop.description}</p>
+
+  
+  </>
+  
+  )
+}
--- src/pages/Meteo.jsx
+++ src/pages/Meteo.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
-import ApiMeteo from '../components/ServizioMeteo/ApiMeteo';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import MeteoCard from '../components/ServizioMeteo/MeteoCard';
+
+
 
 
 export default function Meteo() {
+ 
+  const [data, setData] = useState([]);
 
 
-
-
-
-
-  const meteoClient = ApiMeteo();
   useEffect(() => {
-    meteoClient.fetchMeteo()
-    console.log(meteoClient);
+    (async () => {
+     const res = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=Rome,Italy&appid=5927eede81005116559ee463a04139b8");
+     setData(res.data.weather);
+     console.log(res.data.weather)
+    })();
+   }, []);
 
-  }, [])
+ 
+   const dati=JSON.stringify(data);
+   console.log(dati);
+  
+  return(
+    /* [{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}] */
+  <>
+    
+      {
+        data.map((item)=>(
+          <MeteoCard
+          key={item.id}
+          main={item.main}
+          id={item.id}
+          icon={item.icon}
+          description={item.description}
+          
 
-
-
-  return (
-    <>
-
-
+          />
+  ))
+      }
+    
+       
+     
     </>
-  )
-}
+
+  
+  );
+
+
+
+  }
+  
+
