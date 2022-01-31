045aaa7e76675010a8a7e6a6efeb28035a3490a1
unknown <massimiliano.carboni96@gmail.com>
2022-01-31 13:27:32 +0100

    Netlify toml

--- src/pages/Meteo.jsx
+++ src/pages/Meteo.jsx
@@ -10,41 +10,47 @@ import styled from 'styled-components';
 export default function Meteo() {
 
   const [data, setData] = useState([]);
-const[city,setCity]=useState();
+  const [city, setCity] = useState('');
 
-  const SearchCity=styled.input`
+  const SearchCity = styled.input`
   
   `
 
 
 
 
-  useEffect(() => {
-    (async () => {
-      try{const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Roma&appid=5927eede81005116559ee463a04139b8`);
-      return res.data.weather;
-    }catch(errors){
-      console.log(errors);
-    }
+  
+    useEffect(() => {
+      (async () => {
+        try {
+          setCity('Rome');
+          const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q='${city}'&appid=5927eede81005116559ee463a04139b8`);
+          return res.data.weather;
+        } catch (errors) {
+          console.log(errors);
+        }
 
-      
 
-    })().then((res)=>setData(res));
-  }, []);
-  console.log(data)
 
+      })().then((res) => setData(res));
+    }, []);
+    
   
 
-  
+
+
+
 
 
   return (
     /* [{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}] */
     <>
-    
-    <SearchCity value={city} onInput={e=>setCity(e.target.city)}/>
+
+      <SearchCity value={city} onInput={e => { setCity(e.target.value) }}
+      />
+      
       {
-        
+
         data.map((item) => (
           <MeteoCard
             icon={item.icon}
@@ -52,7 +58,7 @@ const[city,setCity]=useState();
             main={item.main}
             id={item.id}
             description={item.description}
-            
+
 
           />
         ))
